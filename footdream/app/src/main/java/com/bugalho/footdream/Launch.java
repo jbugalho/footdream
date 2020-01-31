6c7ab262bfbc2ab6cd3702669763d8da8549858e
bugalho <joao.bugalho@outlook.com>
2020-01-31 17:59:53 +0000

    Fragmento de Registo e Login Adicionados

--- footdream/app/src/main/java/com/bugalho/footdream/Launch.java
+++ footdream/app/src/main/java/com/bugalho/footdream/Launch.java
@@ -5,19 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.bugalho.footdream.ui.launch.LaunchFragment;
+import com.bugalho.footdream.ui.register.RegisterFragment;
 
 public class Launch extends AppCompatActivity {
 
 
-    private boolean register = false;
+    private boolean register = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.launch_activity);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, LaunchFragment.newInstance())
-                    .commitNow();
+
+        if (!register) {
+            if (savedInstanceState == null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.container, LaunchFragment.newInstance())
+                        .commitNow();
+            }
+        }else{
+            if (savedInstanceState == null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.container, RegisterFragment.newInstance())
+                        .commitNow();
+            }
         }
     }
 }
