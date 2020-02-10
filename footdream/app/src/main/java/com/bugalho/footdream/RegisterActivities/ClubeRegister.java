e13dfacfc2099d234a974bdd06d85c49ed1571d8
bugalho <joao.bugalho@outlook.com>
2020-02-10 23:20:34 +0000

    loading adicionado ao registo

--- footdream/app/src/main/java/com/bugalho/footdream/RegisterActivities/ClubeRegister.java
+++ footdream/app/src/main/java/com/bugalho/footdream/RegisterActivities/ClubeRegister.java
@@ -12,6 +12,7 @@ import android.widget.EditText;
 import com.bugalho.footdream.Helper.DatabaseBuilder;
 import com.bugalho.footdream.Helper.OnDatabaseBuilderQueryExecuteListener;
 import com.bugalho.footdream.Helper.QueryMode;
+import com.bugalho.footdream.LoadingDialog;
 import com.bugalho.footdream.MainActivity;
 import com.bugalho.footdream.R;
 import com.bugalho.footdream.UserClass.UserType;
@@ -22,6 +23,7 @@ public class ClubeRegister extends AppCompatActivity {
     private EditText nomeClube;
     private EditText emailInsert;
     private EditText password;
+    private LoadingDialog loadingDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,27 +34,35 @@ public class ClubeRegister extends AppCompatActivity {
         nomeClube = findViewById(R.id.nomeClube);
         emailInsert = findViewById(R.id.emailInsert);
         password =  findViewById(R.id.passwordInsert);
+        loadingDialog = new LoadingDialog(this);
 
         inserir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                loadingDialog.startLoadingAnimation();
                 DatabaseBuilder queryAllUsers = new DatabaseBuilder(QueryMode.WRITE,
-                        "INSERT INTO Clubes (nome_clube,email,password) VALUES ('" + nomeClube.getText() +"','" + emailInsert.getText() + "','" + password.getText() + "')");
+                        "INSERT INTO Clubes (nome_clube,email,password) VALUES ('" + nomeClube.getText() + "','" + emailInsert.getText() + "','" + password.getText() + "')");
+
 
                 queryAllUsers.execute(new OnDatabaseBuilderQueryExecuteListener() {
                     @Override
                     public void OnGetResultHandler(Object resultSet) {
                         Integer rowsAffected = (Integer) resultSet;
-                        if(rowsAffected > 0) Log.d("user_add", "utilizador adicionado com sucesso");
-                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                        intent.putExtra("tipo", UserType.Clube);
-                        intent.putExtra("nome", nomeClube.getText().toString());
-                        intent.putExtra("descricao", " ");
-                        startActivity(intent);
-                        finish();
+
+                        if (rowsAffected > 0) {
+                            Log.d("user_add", "utilizador adicionado com sucesso");
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            intent.putExtra("tipo", UserType.Clube);
+                            intent.putExtra("nome", nomeClube.getText().toString());
+                            intent.putExtra("descricao", " ");
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            nomeClube.setError("Este clube já se encontra registado");
+                            emailInsert.setError("Este clube já se encontra registado");
+                        }
                     }
                 });
-
             }
         });
 
